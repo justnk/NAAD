f8b4489ce63b12d3b4196d2d92de584d8aae4ff9
Naman Khandelwal <namankhandelwal96@gmail.com>
2021-04-11 19:41:49 +0530

    Initial Commit

---
+++ src/com/streamliners/task0/DecisionMakingAndLoops/AtoZ.java
@@ -0,0 +1,9 @@
+package com.streamliners.task0.DecisionMakingAndLoops;
+
+public class AtoZ {
+    public static void main(String[] args) {
+        char ch;
+        for (ch = 'A' ; ch <= 'Z' ; ch ++)
+            System.out.print(ch + " ");
+    }
+}
