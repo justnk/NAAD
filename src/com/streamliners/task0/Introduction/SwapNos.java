f080e0531231ea8c655ff3259db225cb728af8f9
Naman Khandelwal <namankhandelwal96@gmail.com>
2021-04-11 16:42:36 +0530

    Initial Commit

---
+++ src/com/streamliners/task0/Introduction/SwapNos.java
@@ -0,0 +1,12 @@
+package com.streamliners.task0.Introduction;
+
+public class SwapNos {
+    public static void main(String[] args) {
+        int a=10,b=5,temp;
+        System.out.println("a = " + a + " and b = " + b);
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("a = " + a + " and b = " + b);
+    }
+}
