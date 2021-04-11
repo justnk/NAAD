09fd7fab35304a21e3443307a89fcd1205e894a5
Naman Khandelwal <namankhandelwal96@gmail.com>
2021-04-11 16:38:58 +0530

    Initial Commit

---
+++ src/com/streamliners/task0/Introduction/QuotientRemainder.java
@@ -0,0 +1,10 @@
+package com.streamliners.task0.Introduction;
+
+public class QuotientRemainder {
+    public static void main(String[] args) {
+        int a=50,b=7,q,r;
+        q=a/b;
+        r=a%b;
+        System.out.println("The quotient of " + a + " and " + b + " is " + q + " remainder is " + r);
+    }
+}
