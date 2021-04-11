0c708778b139beba38c517cc87ff7bd0465e754c
Naman Khandelwal <namankhandelwal96@gmail.com>
2021-04-11 17:56:54 +0530

    Initial Commit

---
+++ src/com/streamliners/task0/Introduction/Roundoff.java
@@ -0,0 +1,9 @@
+package com.streamliners.task0.Introduction;
+
+public class Roundoff {
+    public static void main(String[] args) {
+        double Num = 9.9907;
+        System.out.println(Num + "rounded off to 2 places is ");
+        System.out.format("%.2f",Num);
+    }
+}
