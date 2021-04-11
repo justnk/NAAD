ce6f3f672f7dc211d21f24b096d19823453ea39a
Naman Khandelwal <namankhandelwal96@gmail.com>
2021-04-11 18:07:48 +0530

    Initial Commit

---
+++ src/com/streamliners/task0/TypeConversion/StrToInt.java
@@ -0,0 +1,11 @@
+package com.streamliners.task0.TypeConversion;
+
+public class StrToInt {
+    public static void main(String[] args) {
+        String str1="96",str2="90";
+        int num1,num2;
+        num1 = Integer.parseInt(str1);
+        num2 = Integer.parseInt(str2);
+        System.out.println(num1 + " " + num2);
+    }
+}
