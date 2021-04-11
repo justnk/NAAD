c0e32c3001a8c61e6ad72a0f8894897661813c33
Naman Khandelwal <namankhandelwal96@gmail.com>
2021-04-11 16:16:29 +0530

    Initial Commit

---
+++ src/com/streamliners/task0/Introduction/MultiplyFlt.java
@@ -0,0 +1,9 @@
+package com.streamliners.task0.Introduction;
+
+public class MultiplyFlt {
+    public static void main(String[] args) {
+        float a = 5 , b = 3;
+        float mul = a*b;
+        System.out.println(a + " x " + b + " = " + mul);
+    }
+}
