7e2218bb234158cbac764e641bf395d5dca9767f
Naman Khandelwal <namankhandelwal96@gmail.com>
2021-04-11 18:10:38 +0530

    Initial Commit

---
+++ src/com/streamliners/task0/TypeConversion/IntToStr.java
@@ -0,0 +1,11 @@
+package com.streamliners.task0.TypeConversion;
+
+public class IntToStr {
+    public static void main(String[] args) {
+        int num1=96,num2=96;
+        String str1,str2;
+        str1 = String.valueOf(num1);
+        str2 = String.valueOf(num2);
+        System.out.println(str1 + " " + str2);
+    }
+}
